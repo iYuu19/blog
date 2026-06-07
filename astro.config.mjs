ef23afc2ad5ee8a8fba6723b7db2dea56d1f09e9
iYuu19 <3118206979@qq.com>
2026-06-07 20:22:25 +0800

    Refine blog layout and metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://iyu-challenge-blog.pages.dev",
+  site: "https://blog-cx8.pages.dev",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
